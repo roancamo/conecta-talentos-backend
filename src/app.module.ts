5018377d8edecbde6e6bf246123aaa02524a076e
roancamo <rodrigo.cabello.1979@gmail.com>
2024-07-18 20:50:04 -0400

    Módulo de Estudiantes Completo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EstudiantesController } from './estudiantes/estudiantes/estudiantes.controller';
-import { EstudiantesService } from './estudiantes/estudiantes/estudiantes.service';
+import { EstudiantesController } from './estudiantes/estudiantes.controller';
+import { EstudiantesService } from './estudiantes/estudiantes.service';
 @Module({
   imports: [],
   controllers: [AppController,EstudiantesController],
