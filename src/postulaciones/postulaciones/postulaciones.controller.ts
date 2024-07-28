d8fd24f7031a9b8e8986605a283674630a7c17c3
roancamo <rodrigo.cabello.1979@gmail.com>
2024-07-28 19:46:07 -0400

    Modulo de Ofertas laborales

--- src/postulaciones/postulaciones/postulaciones.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('postulaciones')
-export class PostulacionesController {}
--- src/postulaciones/postulaciones/postulaciones.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Postulacion } from 'src/model/postulacion'; 
-@Injectable()
-export class PostulacionesService {
-
-    private postulaciones :Postulacion[] = [];
-}
